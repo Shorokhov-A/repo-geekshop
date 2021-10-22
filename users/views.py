05188a21488cd44ac90e0ddce3218b9779086b04
Shorockhov-A <andr2409@bk.ru>
2021-10-22 15:33:47 +0300

    added profile controller

--- users/views.py
+++ users/views.py
@@ -3,7 +3,7 @@ from django.urls import reverse
 from django.contrib import auth
 from django.contrib import messages
 
-from users.forms import UserLoginForm, UserRegistrationForm
+from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
 
 
 def login(request):
@@ -40,7 +40,14 @@ def registration(request):
 
 
 def profile(request):
-    context = {'title': 'GeekShop - Профиль'}
+    if request.method == 'POST':
+        form = UserProfileForm(instance=request.user, files=request.FILES, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('users:profile'))
+    else:
+        form = UserProfileForm(instance=request.user)
+    context = {'title': 'GeekShop - Профиль', 'form': form}
     return render(request, 'users/profile.html', context)
 
 
