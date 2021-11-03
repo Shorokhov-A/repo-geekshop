2197790dcb8cec87eacdc222717922372079082d
Shorockhov-A <andr2409@bk.ru>
2021-11-03 15:17:09 +0300

    added ListView with Paginator to products app

--- products/views.py
+++ products/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic.base import TemplateView
+from django.views.generic.list import ListView
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from products.models import ProductCategory, Product
 
@@ -13,6 +14,20 @@ class IndexTemplateView(TemplateView):
         context = super(IndexTemplateView, self).get_context_data(**kwargs)
         context['title'] = 'GeekShop'
         context['header'] = 'GeekShop Store'
+        return context
+
+
+class ProductsListView(ListView):
+    model = Product
+    paginate_by = 3
+    template_name = 'products/products.html'
+    context_object_name = 'products'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(ProductsListView, self).get_context_data(**kwargs)
+        context['title'] = 'GeekShop - Каталог'
+        context['header'] = 'GeekShop'
+        context['categories'] = ProductCategory.objects.all()
         return context
 
 
